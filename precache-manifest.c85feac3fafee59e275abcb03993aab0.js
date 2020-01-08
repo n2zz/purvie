057c47e3a0b674c56f1b55fe6e24ccd593c4bd67
n2zz <n2zz@nate.com>
2020-01-08 21:08:00 +0900

    Updates

---
+++ precache-manifest.c85feac3fafee59e275abcb03993aab0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "402de86312d3e83fbeafdff742ba48e5",
+    "url": "/purvie/index.html"
+  },
+  {
+    "revision": "07726759234e32e7f4ce",
+    "url": "/purvie/static/css/main.f8d3bcc8.chunk.css"
+  },
+  {
+    "revision": "7a8a01a62d90437792dd",
+    "url": "/purvie/static/js/2.807fa7e2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/purvie/static/js/2.807fa7e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "07726759234e32e7f4ce",
+    "url": "/purvie/static/js/main.dffc4f96.chunk.js"
+  },
+  {
+    "revision": "4c42463428561ed46df5",
+    "url": "/purvie/static/js/runtime-main.cb693675.js"
+  },
+  {
+    "revision": "c98c5c9f73e5c761890add95a9b47269",
+    "url": "/purvie/static/media/bg2.c98c5c9f.png"
+  }
+]);
