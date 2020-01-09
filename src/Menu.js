dde3f4ddb8ebb40c2f3d7fb1965973e0aa9991b2
n2zz <n2zz@nate.com>
2020-01-09 09:57:52 +0900

    설정변경

--- src/Menu.js
+++ src/Menu.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
+import { Helmet } from "react-helmet";
 import { Route } from "react-router-dom";
 import logo from "./images/logo-square.png";
 import App from "./App";
 import Menu1 from "./Menu1";
 import MenuLink from "./MenuLink";
 
+const title = "PURVIE";
+
 class Menu extends Component {
   render() {
     return (
       <div>
+        <Helmet>
+          <title>{title}</title>
+          <link
+            rel="icon"
+            type="image/png"
+            href={logo}
+            sizes="16x16"
+            alt="favicon"
+          />
+        </Helmet>
         <div>
           <img src={logo} align="left" width="50" height="50"></img>
           <h1>PURVIE</h1>
