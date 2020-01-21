4b9e19d39def2fa84f96d001c239e66c3bae1041
byeongjoon_kim <byeongjoon_kim@kolon.com>
2020-01-21 14:41:43 +0900

    네이버API 호출추가

--- src/Menu.js
+++ src/Menu.js
@@ -6,7 +6,7 @@ import App from "./App";
 import Menu1 from "./Menu1";
 import Menu2 from "./Menu2";
 import MenuLink from "./MenuLink";
-import MainStillcutSlider from "./MainStillcutSlider"
+import MainStillcutSlider from "./MainStillcutSlider";
 import "./Menu.css";
 
 const title = "PURVIE";
@@ -14,48 +14,46 @@ const title = "PURVIE";
 class Menu extends Component {
   render() {
     return (
-        <div>
-          <Helmet>
-            <title>{title}</title>
-            <link
-              rel="icon"
-              type="image/png"
-              href={logo}
-              sizes="16x16"
-              alt="favicon"
-            />
-          </Helmet>
-          <BrowserRouter>
-            <div className="menu_header">
-              <div className="menu_head">
-                <div className="menu_icon">
-                  <img src={logo} className="img_title_icon"></img>
-                </div>
-                <div className="menu_title">
-                  PURVIE
-                </div>
+      <div>
+        <Helmet>
+          <title>{title}</title>
+          <link
+            rel="icon"
+            type="image/png"
+            href={logo}
+            sizes="16x16"
+            alt="favicon"
+          />
+        </Helmet>
+        <BrowserRouter>
+          <div className="menu_header">
+            <div className="menu_head">
+              <div className="menu_icon">
+                <img src={logo} className="img_title_icon"></img>
               </div>
-              <div className="menu1">
-                <MenuLink />
-              </div>
-              <div className="search_group">
-                <div className="search">
-                  <input type="text" class="search_input" placeholder="Search"/>
-                  <div class="search_line"></div>
-                  <div class="search_close"></div>
-                </div>
-              </div>
-              <MainStillcutSlider/>
-              <div>
-                <Switch> 
-                  <Route exact path="/"component={Menu1} />
-                  <Route path="/menu1"component={Menu1} />
-                  <Route path="/menu2" component={Menu2} />
-                </Switch>
+              <div className="menu_title">PURVIE</div>
+            </div>
+            <div className="menu1">
+              <MenuLink />
+            </div>
+            <div className="search_group">
+              <div className="search">
+                <input type="text" class="search_input" placeholder="Search" />
+                <div class="search_line"></div>
+                <div class="search_close"></div>
               </div>
             </div>
-          </BrowserRouter>
-        </div>
+          </div>
+          <MainStillcutSlider />
+          <div>
+            <Switch>
+              <Route exact path="/" component={Menu1} />
+              <Route path="/menu1" component={Menu1} />
+              <Route path="/menu2" component={Menu2} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </div>
     );
   }
 }
--- src/Menu2.js
+++ src/Menu2.js
@@ -1,25 +1,21 @@
 import React, { Component } from "react";
-import MovieDataLoader from './loader/MovieDataLoader';
-import "./Menu.css"
+import MovieDataLoader from "./loader/MovieDataLoader";
+import "./Menu.css";
 
 class Menu2 extends Component {
   static arrBoxofficeData = null;
 
-  constructor(props)
-  {
+  constructor(props) {
     super(props);
     this.ldrMovieData = new MovieDataLoader();
   }
 
-  drawBoxOfficeList(arrBoxOfficeData)
-  {
+  drawBoxOfficeList(arrBoxOfficeData) {
     let nFirst = 0;
     let divTest = document.getElementById("test");
-    
-    if(arrBoxOfficeData != null)
-    {
-      for(nFirst = 0; nFirst < arrBoxOfficeData.length; nFirst++)
-      {
+
+    if (arrBoxOfficeData != null) {
+      for (nFirst = 0; nFirst < arrBoxOfficeData.length; nFirst++) {
         let divMovie = document.createElement("div");
         let objBOData = arrBoxOfficeData[nFirst];
 
@@ -33,11 +29,11 @@ class Menu2 extends Component {
         // movie title
         let divTitle = document.createElement("div");
         divTitle.className = "title_div";
-        divTitle.innerHTML = '<' + objBOData.movie_title + '>';
+        divTitle.innerHTML = "<" + objBOData.movie_title + ">";
         // movie actor
         let divActor = document.createElement("div");
         divActor.className = "actor_div";
-        divActor.innerHTML = objBOData.open_year.replace(/-/gi, '.');
+        divActor.innerHTML = objBOData.open_year.replace(/-/gi, ".");
         // review
         let divReview1 = document.createElement("div");
         divReview1.className = "review_div";
@@ -58,15 +54,13 @@ class Menu2 extends Component {
         divMovie.id = "rdiv";
         divImage.id = "Ldiv";
         divInfo.id = "Rdiv";
-        
+
         divTest.appendChild(divMovie);
       }
-
     }
   }
 
-  getBoxofficeList(bDaily)
-  {
+  getBoxofficeList(bDaily) {
     let objThis = this;
 
     this.ldrMovieData.search_condition.item_per_page = 5;
@@ -75,20 +69,20 @@ class Menu2 extends Component {
     //this.ldrMovieData.search_condition.product_year = "2017";
     //this.ldrMovieData.search_condition.movie_title = "백두산";
 
-    this.ldrMovieData.getBoxOfficeListWithPoster().then(
-        function(arrBOData)
-        {
-          if(arrBOData != null)
-          {
-            console.log("리스트 목록 길이 : " + arrBOData.length);
-            objThis.drawBoxOfficeList(arrBOData);
+    this.ldrMovieData
+      .getBoxOfficeListWithPoster()
+      .then(function(arrBOData) {
+        if (arrBOData != null) {
+          console.log("리스트 목록 길이 : " + arrBOData.length);
+          for (const m in arrBOData) {
+            console.log(m);
           }
+          objThis.drawBoxOfficeList(arrBOData);
         }
-    ).catch(function(e)
-        {
-          console.log("Error Massage : " + e);
-        }
-    );
+      })
+      .catch(function(e) {
+        console.log("Error Massage : " + e);
+      });
   }
 
   componentDidMount() {
@@ -96,10 +90,7 @@ class Menu2 extends Component {
   }
 
   render() {
-    return (
-      <div id="test">
-      </div>
-    );
+    return <div id="test"></div>;
   }
 }
 
