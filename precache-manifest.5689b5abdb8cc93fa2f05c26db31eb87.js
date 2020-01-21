76a89ea7c3cfe515df4dcb324db0dff84abf3727
byeongjoon_kim <byeongjoon_kim@kolon.com>
2020-01-21 18:18:02 +0900

    Updates

---
+++ precache-manifest.5689b5abdb8cc93fa2f05c26db31eb87.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d9f38726482ad6c9574b0521e93886",
+    "url": "/purvie/index.html"
+  },
+  {
+    "revision": "2c5e7933ae38b2d31277",
+    "url": "/purvie/static/css/main.4def86aa.chunk.css"
+  },
+  {
+    "revision": "da43b793d5b1b51ad58c",
+    "url": "/purvie/static/js/2.e689e73f.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/purvie/static/js/2.e689e73f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2c5e7933ae38b2d31277",
+    "url": "/purvie/static/js/main.dd8efcc5.chunk.js"
+  },
+  {
+    "revision": "4c42463428561ed46df5",
+    "url": "/purvie/static/js/runtime-main.cb693675.js"
+  },
+  {
+    "revision": "a70857e6e4e14337824725c15f2a5e34",
+    "url": "/purvie/static/media/NanumSquare_acB.a70857e6.ttf"
+  },
+  {
+    "revision": "0e1f2365ab61e1ea88d2fbdef3952fdb",
+    "url": "/purvie/static/media/NanumSquare_acR.0e1f2365.ttf"
+  },
+  {
+    "revision": "0e451c0beb2d184d45953f811389635b",
+    "url": "/purvie/static/media/Typo_SsangmunDongB.0e451c0b.ttf"
+  },
+  {
+    "revision": "6b11a4c30f9c750a94e556c8ac7132bf",
+    "url": "/purvie/static/media/dovemayo.6b11a4c3.otf"
+  },
+  {
+    "revision": "e5a5e9c2d2ba47572dc0fa64a5c7bb7e",
+    "url": "/purvie/static/media/logo-square.e5a5e9c2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/purvie/precache-manifest.a30b0683d235d9652e18db7417f1527d.js"
+  "/purvie/precache-manifest.5689b5abdb8cc93fa2f05c26db31eb87.js"
 );
 
 self.addEventListener('message', (event) => {
